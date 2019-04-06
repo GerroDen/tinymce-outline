7a319cf53fc92ca01c43f3c86bb32aaf182ce1c5
Lars-Erik Kimmel <gerro_den@gmx.de>
2019-04-06 21:05:07 +0200

    Listen to resize events and extract outline class

--- src/index.js
+++ src/index.js
@@ -1,65 +1,91 @@
 import "./style.less";
 import tinymce from "tinymce";
 
-function isInViewport(element) {
-	const { top, bottom } = element.getBoundingClientRect();
-	const vHeight = element.ownerDocument.documentElement.clientHeight;
-	return (top > 0 || bottom > 0) && top < vHeight;
-}
-
-function buildOutline(editor) {
-	const outline = editor.dom.create("ul", { class: "tinymce-outline" });
-	editor.$("h1, h2, h3, h4, h5, h6").each((index, element) => {
-		const li = editor.dom.add(
-			outline,
-			"li",
-			{
-				class: [
-					"tinymce-outline-" + element.tagName.toLowerCase(),
-					isInViewport(element) ? "is-visible" : ""
-				].join(" "),
-				title: element.innerText
-			},
-			element.innerText
-		);
-		li.addEventListener("click", () => {
-			element.scrollIntoView({ behavior: "smooth" });
-		});
-	});
-	return outline;
-}
-
-let sidebarPanel;
-
-function updateSidebarPanel(editor) {
-	const outline = buildOutline(editor);
-	while (sidebarPanel.firstChild) {
-		sidebarPanel.removeChild(sidebarPanel.firstChild);
+class Outline {
+	constructor(editor, sidebarElement) {
+		this.editor = editor;
+		this.sidebarPanel = sidebarElement;
 	}
-	editor.dom.add(sidebarPanel, outline);
+
+	buildOutline() {
+		const outline = this.editor.dom.create("ul", { class: "tinymce-outline" });
+		this.editor.$("h1, h2, h3, h4, h5, h6").each((index, element) => {
+			const li = this.editor.dom.add(
+				outline,
+				"li",
+				{
+					class: [
+						"tinymce-outline-" + element.tagName.toLowerCase(),
+						this.isElementInViewport(element) ? "is-visible" : ""
+					].join(" "),
+					title: element.innerText
+				},
+				element.innerText
+			);
+			li.addEventListener("click", () => {
+				element.scrollIntoView({ behavior: "smooth" });
+			});
+		});
+		return outline;
+	}
+
+	isElementInViewport(element) {
+		const { top, bottom } = element.getBoundingClientRect();
+		const vHeight = element.ownerDocument.documentElement.clientHeight;
+		return (top > 0 || bottom > 0) && top < vHeight;
+	}
+
+	updateSidebarPanel() {
+		if (this.isHiddenElement(this.sidebarPanel)) {
+			return;
+		}
+		const outline = this.buildOutline(this.editor);
+		this.clearSidebarPanel();
+		this.editor.dom.add(this.sidebarPanel, outline);
+	}
+
+	clearSidebarPanel() {
+		while (this.sidebarPanel.firstChild) {
+			this.sidebarPanel.removeChild(this.sidebarPanel.firstChild);
+		}
+	}
+
+	isHiddenElement(element) {
+		return element.offsetParent === null;
+	}
+
+	init() {
+		this.editor
+			.getWin()
+			.addEventListener(
+				"scroll",
+				tinymce.util.Delay.debounce(() => this.updateSidebarPanel(), 100)
+			);
+		this.editor.on(
+			"Change",
+			tinymce.util.Delay.debounce(() => this.updateSidebarPanel(), 100)
+		);
+		window.addEventListener(
+			"resize",
+			tinymce.util.Delay.debounce(() => this.updateSidebarPanel(), 100)
+		);
+	}
 }
 
 tinymce.PluginManager.add("outline", editor => {
 	editor.on("Init", () => {
 		editor.dom.loadCSS("main.css");
-		editor
-			.getWin()
-			.addEventListener(
-				"scroll",
-				tinymce.util.Delay.debounce(() => updateSidebarPanel(editor), 10)
-			);
 	});
+	let outline;
 	editor.addSidebar("mysidebar", {
 		tooltip: "My sidebar",
 		icon: "settings",
 		onrender(api) {
-			sidebarPanel = api.element();
-			updateSidebarPanel(editor);
+			outline = new Outline(editor, api.element());
+			outline.init();
 		},
-		onshow(api) {},
-		onhide(api) {}
-	});
-	editor.on("Change", () => {
-		updateSidebarPanel(editor);
+		onshow() {
+			outline.updateSidebarPanel();
+		}
 	});
 });
